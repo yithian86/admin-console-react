4500089ca4b742257fd1778a1f8d593aae6ff542
yithian86 <cinelli.alex.it@gmail.com>
2024-12-26 13:27:59 +0100

    fix: fixed issues related to the active menu items

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 // React imports
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
+import { Route, Routes, Link, useLocation } from 'react-router-dom'
 import { useState } from 'react';
 // Antd imports
 import type { MenuProps, MenuTheme } from 'antd';
@@ -15,9 +15,19 @@ import ImportExport from './pages/i18n-import-export';
 
 
 export default function App() {
+  // Hook from react-router-dom to get current path
+  const location = useLocation();
+  // State to manage the current route
+  const getCurrentLocation = () => {
+    console.debug(location.pathname);
+    return location.pathname;
+  }
+  const [currentRoute, setCurrentRoute] = useState(getCurrentLocation());
+  // State to manage the theme of the menu
   const [menuTheme, setMenuTheme] = useState<MenuTheme>('dark');
+  // State to manage the collapse of the menu
   const [collapsed, setCollapsed] = useState(false);
-  const [currentRoute, setCurrentRoute] = useState('dashboard');
+
 
   const changeTheme = (value: boolean) => {
     setMenuTheme(value ? 'dark' : 'light');
@@ -33,70 +43,66 @@ export default function App() {
 
   const items: MenuProps['items'] = [
     {
-      key: 'dashboard',
+      key: '/',
       icon: <DashboardOutlined />,
       label: <Link to="/">Dashboard</Link>
     },
     {
-      key: 'i18n',
+      key: '/i18n',
       icon: <AlipayOutlined />,
       label: <Link to="/i18n" >Globalization</Link>,
       children: [
-        { key: 'languages', label: <Link to="/languages-manager" >Languages</Link> },
-        { key: 'labels', label: <Link to="/labels" >Labels</Link> },
-        { key: 'importExport', label: <Link to="/import-export" >Import / Export</Link> }
+        { key: '/languages-manager', label: <Link to="/languages-manager" >Languages</Link> },
+        { key: '/labels', label: <Link to="/labels" >Labels</Link> },
+        { key: '/import-export', label: <Link to="/import-export" >Import / Export</Link> }
       ]
     }
   ];
 
   return (
-    <>
-      <Router>
-        <div className="AdminConsole">
+    <div className="AdminConsole">
 
-          {/* MENU */}
-          <div className={"AdminConsole__menuWrapper AdminConsole__menuWrapper--" + menuTheme}>
-            <Button color="default" variant="text" onClick={toggleCollapsed} style={{ width: '100%', height: '48px' }}>
-              <img src="./src/assets/react.svg"/>
-            </Button>
+      {/* MENU */}
+      <div className={"AdminConsole__menuWrapper AdminConsole__menuWrapper--" + menuTheme}>
+        <Button color="default" variant="text" onClick={toggleCollapsed} style={{ width: '100%', height: '48px' }}>
+          <img src="./src/assets/react.svg" />
+        </Button>
 
-            <Menu
-              mode="inline"
-              inlineCollapsed={collapsed}
-              theme={menuTheme}
-              items={items}
-              onClick={onClick}
-              selectedKeys={[currentRoute]}
-              getPopupContainer={(node) => node.parentNode as HTMLElement}
-            />
-          </div>
+        <Menu
+          mode="inline"
+          inlineCollapsed={collapsed}
+          theme={menuTheme}
+          items={items}
+          onClick={onClick}
+          selectedKeys={[currentRoute]}
+          defaultOpenKeys={['/i18n']}
+          getPopupContainer={(node) => node.parentNode as HTMLElement}
+        />
+      </div>
 
-          {/* CONTENT */}
-          <div className="AdminConsole__content">
-            <div className="AdminConsole__header">
-              {/* Theme switch */}
-              <Switch
-                checked={menuTheme === 'dark'}
-                onChange={changeTheme}
-                checkedChildren="Dark"
-                unCheckedChildren="Light"
-              />
-            </div>
-
-            {/* Routes */}
-            <div className='AdminConsole__page'>
-              <Routes>
-                <Route path="/" element={<Dashboard />} />
-                <Route path="/i18n" element={<Globalization />} />
-                <Route path="/languages-manager" element={<Languages />} />
-                <Route path="/labels" element={<Labels />} />
-                <Route path="/import-export" element={<ImportExport />} />
-              </Routes>
-            </div>
-          </div>
+      {/* CONTENT */}
+      <div className="AdminConsole__content">
+        <div className="AdminConsole__header">
+          {/* Theme switch */}
+          <Switch
+            checked={menuTheme === 'dark'}
+            onChange={changeTheme}
+            checkedChildren="Dark"
+            unCheckedChildren="Light"
+          />
         </div>
-      </Router>
 
-    </>
+        {/* Routes */}
+        <div className='AdminConsole__page'>
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/i18n" element={<Globalization />} />
+            <Route path="/languages-manager" element={<Languages />} />
+            <Route path="/labels" element={<Labels />} />
+            <Route path="/import-export" element={<ImportExport />} />
+          </Routes>
+        </div>
+      </div>
+    </div>
   )
 }
